c3c9e91e8c61ccc82867b0393458ca5ddb38d6d6
jeff-dillon <dillonjg@gmail.com>
2025-01-08 12:58:59 -0600

    Completed Step 1

--- js/index.js
+++ js/index.js
@@ -2,3 +2,36 @@
 // that are not declared
 "use strict"
 
+/*
+
+console.log(1 + 1);         // Output: 2
+console.log(1 - 1);          // Output: 0
+console.log(1 + "1");        // Output: 11
+                             // What? Why? Search for the answer
+
+console.log(3 - 4 * 5)       // Output: -17
+console.log((3 - 4) * 5);    // Output: -5
+                             // Remember order of precendence
+
+console.log(6 / 5);          // Output: 1.2
+console.log(6 % 5);          // Output: 1
+                             // Modulus return the remainder. This operator is extremely useful.
+
+console.log(1 == 1);         // Output: true
+console.log(1 == "1");       // Output: true
+console.log(1 === "1");      // Output: false
+
+console.log(1 || 2);         // Output: 1
+console.log(2 || 1);         // Output: 2
+console.log(1 && 2);         // Output: false
+console.log(1 && 1);         // Output: true
+
+let oneAndTwo = 1 && 2;
+console.log(oneAndTwo)
+console.log(typeof oneAndTwo)
+
+console.log(1 && true);
+console.log(2 && false);
+console.log(3 && false);
+
+*/
